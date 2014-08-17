a02e416b8ad3fc6fc38f06a7cca4a8a14809260e
Eli Flesher <eli@eflee.us>
2014-08-17 13:52:36 -0700

    Renaming the NO_BODY template to be the standard META_REDIRECT and the template with the REDIRECT_TEXT to be more clear. Adding the GOOGLE_ANALYTICS template

--- src/stiny/templates.py
+++ src/stiny/templates.py
@@ -26,32 +26,53 @@ from .exceptions import MalformedTemplateConfig, TemplateNotFoundException
 """
 This canned template
 """
-META_REDIRECT = \
-"""
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-    <meta http-equiv="Refresh" content="0; url={{ url.url }}"/>
-</head>
-<body>
-<p>Please click this link if you are not redirected: <a url="{{ url.url }}">link</a>.</p>
-</body>
-</html>
-"""
+META_REDIRECT_REDIRECT_TEXT = \
+    """
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+        <meta http-equiv="Refresh" content="0; url={{ url.url }}"/>
+    </head>
+    <body>
+    <p>Please click this link if you are not redirected: <a url="{{ url.url }}">link</a>.</p>
+    </body>
+    </html>
+    """
 
-META_REDIRECT_NO_BODY = \
-"""
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-    <meta http-equiv="Refresh" content="0; url={{ url.url }}" />
-</head>
-</html>
-"""
+META_REDIRECT = \
+    """
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+        <meta http-equiv="Refresh" content="0; url={{ url.url }}" />
+    </head>
+    </html>
+    """
+
+META_REDIRECT_GOOGLE_ANALYTICS = \
+    """
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+        <meta http-equiv="Refresh" content="0; url={{ url.url }}" />
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', '{{ analytics.id }}', 'auto');
+          ga('send', 'pageview');
+        </script>
+    </head>
+    </html>
+    """
 
 
 def valid_template_value(value):

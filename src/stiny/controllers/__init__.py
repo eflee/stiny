89ffedad6c5a440facc05e11f965275f5add5511
Eli Flesher <eli@eflee.us>
2014-08-17 12:44:25 -0700

    Adding get_controller to controllers module

--- src/stiny/controllers/__init__.py
+++ src/stiny/controllers/__init__.py
@@ -52,8 +52,13 @@ controller
 """
 
 from abc import ABCMeta, abstractmethod
+
 from jinja2 import Template
+
 from ..url import URL
+from ..templates import get_template
+from s3 import S3Controller
+from ..exceptions import UnsupportedStorageTypeException
 
 
 class Controller(object):
@@ -187,3 +192,26 @@ class Controller(object):
         if 'prefix_seperator' not in kwargs:
             kwargs['prefix_separator'] = self.prefix_separator
         return URL(*args, **kwargs)
+
+
+def get_controller(config):
+    """
+    Get a pre-configured storage controller based off the application configuration
+
+    :param config: The StinyConfiguration object used to configure the storage controller
+    :type config: StinyConfiguration
+    :return:
+    """
+    template = get_template(config.get('main', 'template'))
+    storage = config.get('main', 'storage')
+    if storage == 's3':
+        controller = S3Controller(template=template,
+                                  initial_tiny_length=config.get('main', 'min_length'),
+                                  max_retries=config.get('main', 'max_retries'),
+                                  bucket_name=config.get('s3', 'bucket_name'),
+                                  region=config.get('s3', 'region'),
+                                  aws_access_key_id=config.get('s3', 'aws_access_key_id'),
+                                  aws_secret_access_key=config.get('s3', 'aws_secret_access_key'))
+    else:
+        raise UnsupportedStorageTypeException("{store} is an unknown backing store type".format(store=storage))
+    return controller

d89de47b1c2dbf678c2919475cd0b3c4bbb92b2c
Eli Flesher <eli@eflee.us>
2014-08-17 17:41:07 -0700

    Support in get_controller for analytics and compression

--- src/stiny/controllers/__init__.py
+++ src/stiny/controllers/__init__.py
@@ -1,5 +1,6 @@
 from ..templates import get_template
 from ..exceptions import UnsupportedStorageTypeException
+from ..analytics import Analytics
 from s3 import S3Controller
 
 
@@ -12,15 +13,21 @@ def get_controller(config):
     :return:
     """
     template = get_template(config.get('main', 'template'))
+    analytics = Analytics(config.get('main', 'analytics_id')) if config.has('main', 'analytics_id') else None
+
     storage = config.get('main', 'storage')
     if storage == 's3':
+        compress = config.get('s3', 'compress') == 1 if config.has('s3', 'compress') else False
         controller = S3Controller(template=template,
                                   tiny_length=config.get('main', 'min_length'),
                                   max_retries=config.get('main', 'max_retries'),
                                   bucket_name=config.get('s3', 'bucket_name'),
                                   region=config.get('s3', 'region'),
                                   aws_access_key_id=config.get('s3', 'aws_access_key_id'),
-                                  aws_secret_access_key=config.get('s3', 'aws_secret_access_key'))
+                                  aws_secret_access_key=config.get('s3', 'aws_secret_access_key'),
+                                  analytics=analytics,
+                                  compress=compress)
+
     else:
         raise UnsupportedStorageTypeException("{store} is an unknown backing store type".format(store=storage))
     return controller

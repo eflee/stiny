f55bf03306aa3c8468e1f9105eb4a0470ab32cb6
Eli Flesher <eli@eflee.us>
2014-08-18 19:18:29 -0700

    CloudFileController support in get_controller

--- src/stiny/controllers/__init__.py
+++ src/stiny/controllers/__init__.py
@@ -2,6 +2,7 @@ from ..templates import get_template
 from ..exceptions import UnsupportedStorageTypeException
 from ..analytics import Analytics
 from s3 import S3Controller
+from rcf import CloudFilesController
 
 
 def get_controller(config):
@@ -21,13 +22,23 @@ def get_controller(config):
         controller = S3Controller(template=template,
                                   tiny_length=config.get('main', 'min_length'),
                                   max_retries=config.get('main', 'max_retries'),
+                                  analytics=analytics,
                                   bucket_name=config.get('s3', 'bucket_name'),
                                   region=config.get('s3', 'region'),
                                   aws_access_key_id=config.get('s3', 'aws_access_key_id'),
                                   aws_secret_access_key=config.get('s3', 'aws_secret_access_key'),
-                                  analytics=analytics,
                                   compress=compress)
-
+    elif storage == 'rcf':
+        compress = config.get('rcf', 'compress') == 1 if config.has('rcf', 'compress') else False
+        controller = CloudFilesController(template=template,
+                                          tiny_length=config.get('main', 'min_length'),
+                                          max_retries=config.get('main', 'max_retries'),
+                                          analytics=analytics,
+                                          container_name=config.get('rcf', 'container_name'),
+                                          region=config.get('rcf', 'region'),
+                                          rs_username=config.get('rcf', 'username'),
+                                          rs_api_key=config.get('rcf', 'api_key'),
+                                          compress=compress)
     else:
         raise UnsupportedStorageTypeException("{store} is an unknown storage type".format(store=storage))
     return controller
